661cc3c5b90f567fd64fc8bf66ef8b503f6e4186
MelihCelik00 <melihsafa.c@gmail.com>
2022-03-19 10:46:31 +0300

    added first script

---
+++ FPS Example/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Controller Settings")]
+    [SerializeField] private float walkSpeed = 8f;
+    [SerializeField] private float runSpeed = 12f;
+    
+    
+}
