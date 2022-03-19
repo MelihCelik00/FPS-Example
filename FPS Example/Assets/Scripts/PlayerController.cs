d847b26e38a66cbc072fb6722de54e7a09711d7b
MelihCelik00 <melihsafa.c@gmail.com>
2022-03-19 11:08:00 +0300

    player movement and scene update

--- FPS Example/Assets/Scripts/PlayerController.cs
+++ FPS Example/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Controller Settings")]
-    [SerializeField] private float walkSpeed = 8f;
-    [SerializeField] private float runSpeed = 12f;
-    
-    
+    [SerializeField] private float _walkSpeed = 8f;
+    [SerializeField] private float _runSpeed = 12f;
+
+    private CharacterController _characterController;
+
+    private float _currentSpeed = 8f;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        KeyboardInput();
+    }
+
+    private void KeyboardInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _currentSpeed = _runSpeed;
+        }
+        else
+        {
+            _currentSpeed = _walkSpeed;
+        }
+    }
+
+    private void FixedUpdate() // waits physical operations to run
+    {
+        Vector3 localVerticalVector = transform.forward * _verticalInput;
+        Vector3 localHorizontalVector = transform.right * _horizontalInput;
+        Vector3 _movementVector = localHorizontalVector + localVerticalVector ;
+        _movementVector.Normalize();
+        _movementVector *= _currentSpeed * Time.deltaTime;
+        _characterController.Move(_movementVector);
+    }
+
 }
